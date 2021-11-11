031eda242e7a836541ed80d827adb146f579cc60
Leland Curtis <lelandcurtis88@gmail.com>
2021-11-11 11:18:20 -0700

    Add today method spec and update other tetss
    Git
    GitHub
    
    Unstaged Changes
    Stage All
    lib/codex.rb
    lib/cypher.rb
    lib/encoder.rb
    lib/message.rb
    spec/codex_spec.rb
    spec/cypher_spec.rb
    spec/encoder_spec.rb
    spec/enigma_spec.rb
    spec/message_spec.rb
    Staged Changes
    Unstage All
    spec/enigma_spec.rb
    See All Staged Changes
    
    Commit message
    
    Commit to enigma
    72
    
    Add tests for enimga.
    Undo
    14m
    
    Initial commit.
    1h
    
    Initial commit
    1h
    spec/enigma_spec.rb14:27
    LFUTF-8Ruby
    enigmaPublishGitHubGit (9)
    q
    git add spec/enigma_spec.rb!
    exit

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,6 +1,7 @@
+require 'date'
 require './lib/egnima'
-require './lib/codex'
-require './lib/message'
+require './lib/cypher'
+require './lib/encoder'
 
 describe Enigma do
   before(:each) do
@@ -11,9 +12,6 @@ describe Enigma do
       expect(@enigma).to be_a(Enigma)
     end
     it 'has attributes' do
-        expect(@enigma.message).to eq(nil)
-        expect(@enigma.key).to eq(nil)
-        expect(@enigma.date).to eq(nil)
     end
   end
 
@@ -25,6 +23,16 @@ describe Enigma do
       @cyphertext = double('cyphertext')
       allow(@cyphertext).to receive(:decrypt).and_return(@message)
     end
+
+    describe ' #today' do
+      it 'returns a string' do
+        expect(@enigma.today).to be_a(String)
+      end
+      it 'returns correct date' do
+        expect(@enigma.today).to eq('111121')
+      end
+    end
+
     describe ' #encrypt' do
       it 'returns a hash' do
         expect(@enigma.encrypt(@message, @key, @date)).to be_a(Hash)
