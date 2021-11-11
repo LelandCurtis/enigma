690bd3c4d402851520ef9472a9e8192fbbff130a
Leland Curtis <lelandcurtis88@gmail.com>
2021-11-11 11:03:38 -0700

    Add tests for enimga.

---
+++ spec/enigma_spec.rb
@@ -0,0 +1,75 @@
+require './lib/egnima'
+require './lib/codex'
+require './lib/message'
+
+describe Enigma do
+  before(:each) do
+    @enigma = Enigma.new()
+  end
+  describe 'initialize' do
+    it 'exists' do
+      expect(@enigma).to be_a(Enigma)
+    end
+    it 'has attributes' do
+        expect(@enigma.message).to eq(nil)
+        expect(@enigma.key).to eq(nil)
+        expect(@enigma.date).to eq(nil)
+    end
+  end
+
+  describe 'methods' do
+    before(:each) do
+      @message = 'hello world'
+      @key = '02715'
+      @date = '040895'
+      @cyphertext = double('cyphertext')
+      allow(@cyphertext).to receive(:decrypt).and_return(@message)
+    end
+    describe ' #encrypt' do
+      it 'returns a hash' do
+        expect(@enigma.encrypt(@message, @key, @date)).to be_a(Hash)
+      end
+      it 'returns a hash with correct keys' do
+        expected = [:encryption, :key, :date]
+        expect(@enigma.encrypt(@message, @key, @date).keys).to eq(expected)
+      end
+      it 'returns correct key' do
+        expect(@enigma.encrypt(@message, @key, @date)[:key]).to eq(@key)
+      end
+      it 'returns correct date' do
+        expect(@enigma.encrypt(@message, @key, @date)[:date]).to eq(@date)
+      end
+      it 'returns string as encryption' do
+        expect(@enigma.encrypt(@message, @key, @date)[:encryption]).to be_a(String)
+      end
+      it 'returns correct encryption' do
+        message_mock = double('message_mock')
+        allow(message_mock).to receive(:encrypt).and_return(@cyphertext)
+        expected = @cyphertext
+        expect(@enigma.encrypt(message_mock, @key, @date)[:encryption]).to eq(expected)
+      end
+    end
+
+    describe ' #decrypt' do
+      it 'returns a hash' do
+        expect(@enigma.decrypt(@cyphertext, @key, @date)).to be_a(Hash)
+      end
+      it 'returns a hash with correct keys' do
+        expected = [:decryption, :key, :date]
+        expect(@enigma.decrypt(@cyphertext, @key, @date).keys).to eq(expected)
+      end
+      it 'returns correct key' do
+        expect(@enigma.decrypt(@cyphertext, @key, @date)[:key]).to eq(@key)
+      end
+      it 'returns correct date' do
+        expect(@enigma.decrypt(@cyphertext, @key, @date)[:date]).to eq(@date)
+      end
+      it 'returns string as decryption' do
+        expect(@enigma.decrypt(@cyphertext, @key, @date)[:decryption]).to be_a(String)
+      end
+      it 'returns correct decryption' do
+        expect(@enigma.decrypt(@cyphertext, @key, @date)[:decryption]).to eq(@message)
+      end
+    end
+  end
+end
