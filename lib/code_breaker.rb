fff47b3ea54e07f9384e5b6f7079076043e19d79
Leland Curtis <lelandcurtis88@gmail.com>
2021-11-12 17:46:56 -0700

    Rename code_breaker files and create new class and tests.

---
+++ lib/code_breaker.rb
@@ -0,0 +1,19 @@
+require './lib/cypher'
+require './lib/code_breaker'
+require './lib/encoder'
+
+class CodeBreaker < Encoder
+  attr_accessor :shifts
+
+  def initialize
+    @shifts = []
+    @alphabet = @@alphabet
+  end
+
+  def find_shifts(message)
+    correct = [26, 4, 13, 3]
+    last_4_index = create_index_message(message[-4..])
+    shifts = [last_4_index, correct].transpose.map{|pair| (pair[0]-pair[1])%27}
+  end
+
+end
---
+++ spec/code_breaker_spec.rb
@@ -0,0 +1,51 @@
+require 'simplecov'
+SimpleCov.start
+
+require './lib/cypher'
+require './lib/code_breaker'
+require './lib/encoder'
+
+describe CodeBreaker do
+  before(:each) do
+    @message = 'hello world! end'
+    @key = '02715'
+    @date = '040895'
+    @breaker = CodeBreaker.new()
+  end
+
+  describe 'initialize' do
+    it 'exists' do
+      expect(@breaker).to be_a(CodeBreaker)
+    end
+    it 'has attributes' do
+      expect(@breaker.alphabet).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+      expect(@breaker.shifts).to eq([])
+    end
+  end
+
+  describe 'methods' do
+    before(:each) do
+      @crackable_message = 'This is a crackable message end'
+      @cypher = Cypher.new(@key, @date)
+      @encoder = Encoder.new(@cypher)
+      @breaker = CodeBreaker.new()
+      @cyphertext = @encoder.encrypt_message(@crackable_message)
+    end
+
+    describe ' #find_shifts' do
+      it 'returns an array from a message' do
+        expect(@breaker.find_shifts(@crackable_message)).to be_a(Array)
+      end
+      it 'returns an array of 4 values' do
+        expect(@breaker.find_shifts(@crackable_message).count).to eq(4)
+      end
+      it 'returns an array of integers' do
+        expect(@breaker.find_shifts(@crackable_message).all?{|v|v.class == Integer}).to eq(true)
+      end
+      it 'returns the expect shift in proper order' do
+        expected = @cypher.shifts
+        expect(@breaker.find_shifts(@cyphertext)).to eq(expected)
+      end
+    end
+  end
+end
