80a875e78e6382571a285b12c64fcdbd9fd1c06d
Leland Curtis <lelandcurtis88@gmail.com>
2021-11-12 18:00:20 -0700

    Add rotation to find_shifts so shifts align with A,B,C,D shift of cypher.

--- lib/code_breaker.rb
+++ lib/code_breaker.rb
@@ -14,6 +14,7 @@ class CodeBreaker < Encoder
     correct = [26, 4, 13, 3]
     last_4_index = create_index_message(message[-4..])
     shifts = [last_4_index, correct].transpose.map{|pair| (pair[0]-pair[1])%27}
+    shifts.rotate(4 - message.chars.count % 4)
   end
 
 end
