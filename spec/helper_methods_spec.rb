f7287a524e3d7ccedc8d8871d224508d11a57519
Leland Curtis <lelandcurtis88@gmail.com>
2021-11-15 12:19:58 -0700

    Move random from enigma to helper methods.

--- spec/helper_methods_spec.rb
+++ spec/helper_methods_spec.rb
@@ -9,8 +9,17 @@ describe HelperMethods do
     it 'returns a string' do
       expect(today).to be_a(String)
     end
-    it 'returns correct date' do
-      expect(today).to eq('111121')
+    it 'returns a 6 digit date' do
+      expect(today.chars.count).to eq(6)
+    end
+  end
+
+  describe 'random_key' do
+    it 'generates a string' do
+      expect(random_key).to be_a(String)
+    end
+    it 'generates a 5 character string' do
+      expect(random_key.chars.count).to eq(5)
     end
   end
 end
