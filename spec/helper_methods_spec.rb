68dad017e799fd926ad29c56221b78811e8602fa
Leland Curtis <lelandcurtis88@gmail.com>
2021-11-14 18:16:15 -0700

    Update helperMethods tests.

--- spec/helper_methods_spec.rb
+++ spec/helper_methods_spec.rb
@@ -2,14 +2,15 @@ require 'simplecov'
 SimpleCov.start
 
 require './lib/helper_methods'
+include HelperMethods
 
 describe HelperMethods do
   describe ' #today' do
     it 'returns a string' do
-      expect(@enigma.today).to be_a(String)
+      expect(today).to be_a(String)
     end
     it 'returns correct date' do
-      expect(@enigma.today).to eq('111121')
+      expect(today).to eq('111121')
     end
   end
 end
